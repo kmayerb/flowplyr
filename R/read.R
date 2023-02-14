b0b1cce31497d74e48358aa354a7187ee2c21794
kmayerb <kmayerb@gmail.com>
2023-02-14 12:56:57 -0800

    Add initial package structure

---
+++ R/read.R
@@ -0,0 +1,37 @@
+# The functions in this file read standard input files
+
+#' Read .fcs file specifying cell level summary of flow experiment
+#'
+#' @param filepath 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_fcs <- function(filepath){
+  return(1)
+}
+
+#' Read .fcm file specifying sample level summary of flow experiment
+#'
+#' @param filepath 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_fcm<- function(filepath){
+  return(1)
+}
+
+#' Read .xml file with specification of manual gating used a set of the flow experiments
+#'
+#' @param filepath 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_xml<- function(filepath){
+  return(1)
+}
