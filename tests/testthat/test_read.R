0350f31794c0ac15bff3cf06e9e78c191e04281d
kmayerb <kmayerb@gmail.com>
2023-02-14 14:43:16 -0800

    Add dependencies to DESC, testhat

---
+++ tests/testthat/test_read.R
@@ -0,0 +1,12 @@
+test_that("read_fcs exists", {
+  expect_equal(read_fcs(), 1)
+})
+
+test_that("read_fcm exists", {
+  expect_equal(read_fcm(), 1)
+})
+
+test_that("read_xml exists", {
+  expect_equal(read_fcm(), 1)
+})
+
